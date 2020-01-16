aebc91e231760996a180ca59bd2e9be81f8cf6b5
DCCA <dcca12@gmail.com>
2020-01-16 10:56:19 -0800

    add final styles to carousel

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
-var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity(elem, {
-    // options
-    cellAlign: 'left',
-    contain: true
-});
+document.addEventListener(
+    'DOMContentLoaded',
+    function() {
+        // your code goes here
+        var elem = document.querySelector('.main-carousel');
+        var flkty = new Flickity(elem, {
+            // options
+            cellAlign: 'left',
+            contain: true
+        });
 
-// element argument can be a selector string
-//   for an individual element
-var flkty = new Flickity('.main-carousel', {
-    // options
-});
+        // element argument can be a selector string
+        //   for an individual element
+        var flkty = new Flickity('.main-carousel', {
+            // options
+        });
+    },
+    false
+);
