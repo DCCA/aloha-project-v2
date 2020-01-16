7eb8c7d4231d6305ad0e7c12ec09240fc849f6cd
DCCA <dcca12@gmail.com>
2020-01-16 12:59:20 -0800

    add counter to cart

--- js/script.js
+++ js/script.js
@@ -1,19 +1,54 @@
 document.addEventListener(
     'DOMContentLoaded',
     function() {
-        // your code goes here
-        var elem = document.querySelector('.main-carousel');
+        // Flickity Start
+        const elem = document.querySelector('.main-carousel');
         var flkty = new Flickity(elem, {
             // options
             cellAlign: 'left',
             contain: true
         });
 
-        // element argument can be a selector string
-        //   for an individual element
         var flkty = new Flickity('.main-carousel', {
             // options
         });
+
+        // Add Product To Cart
+        let counter = 0;
+        const buttons = document.querySelectorAll('.add-product');
+        const cartCounter = document.getElementsByClassName('cart-counter');
+
+        // Functions
+        function handleCartCounter() {
+            counter++;
+            if (counter > 0) {
+                cartCounter[0].textContent = counter;
+                cartCounter[0].classList.remove('cart-counter-noshow');
+                cartCounter[0].classList.add('cart-counter-show');
+            }
+        }
+
+        function resetCounter() {
+            counter = 0;
+            cartCounter[0].textContent = counter;
+            cartCounter[0].classList.add('cart-counter-noshow');
+            cartCounter[0].classList.remove('cart-counter-show');
+        }
+
+        // Add event listener
+        function start() {
+            cartCounter[0].addEventListener('click', () => {
+                resetCounter();
+            });
+            buttons.forEach(element => {
+                element.addEventListener('click', () => {
+                    handleCartCounter();
+                });
+            });
+        }
+
+        start();
+        console.log(cartCounter);
     },
     false
 );
